be4b66f8ded6f30c397081239b25119ff483f68a
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-09-11 10:27:25 +0800

    fix: Dispose configuration change subscription.

--- src/extension.ts
+++ src/extension.ts
@@ -25,6 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(webviewManager);
   context.subscriptions.push(globalEventHubAdapter);
   context.subscriptions.push(globalModuleManager.api.ScriptService);
+  context.subscriptions.push(globalModuleManager.api.ScriptService);
   const { open: doOpen, reload, close } = webviewManager;
   const open = async () => {
     doOpen();
